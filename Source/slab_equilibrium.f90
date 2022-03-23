0a15af58174d7483f42e9479973e1c598bacc18c
cianciosa <cianciosamr@ornl.gov>
2022-03-23 17:05:07 -0400

    Add slab_equilibrium implimentation.

--- Source/slab_equilibrium.f90
+++ Source/slab_equilibrium.f90
@@ -1,5 +1,5 @@
 !*******************************************************************************
-!>  @file equilibrium_interface.f90
+!>  @file slab_equilibrium.f90
 !>  @brief Pure virtual class to define the interface of an equilibrium.
 !
 !  Note separating the Doxygen comment block here so detailed decription is
@@ -8,28 +8,60 @@
 !>  Defines the interface for an equilibrium.
 !*******************************************************************************
 
-MODULE equilibrium_interface
+MODULE slab_equilibrium
 
-USE, INTRINSIC :: ISO_C_BINDING
-USE asserts
+USE equilibrium_interface
 
 IMPLICIT NONE
 
 !-------------------------------------------------------------------------------
 !>  Base class representing a the interface for an equilibrium
 !-------------------------------------------------------------------------------
-TYPE :: equilibrium_interface_class
+TYPE, EXTENDS(equilibrium_interface_class) :: slab_equilibrium_class
 CONTAINS
-   PROCEDURE, PASS :: get_b_vec => equilibrium_interface_get_b_vec
-   PROCEDURE, PASS :: get_b_mag_jac => equilibrium_interface_get_b_mag_jac
-   PROCEDURE, PASS :: get_n => equilibrium_interface_get_n
-   PROCEDURE, PASS :: get_n_grad => equilibrium_interface_get_n_grad
-   PROCEDURE, PASS :: get_t => equilibrium_interface_get_t
-   PROCEDURE, PASS :: get_t_grad => equilibrium_interface_get_t_grad
-   FINAL           :: equilibrium_interface_destruct
+   PROCEDURE, PASS :: get_b_vec => slab_equilibrium_get_b_vec
+   PROCEDURE, PASS :: get_b_mag_jac => slab_equilibrium_get_b_mag_jac
+   PROCEDURE, PASS :: get_n => slab_equilibrium_get_n
+   PROCEDURE, PASS :: get_n_grad => slab_equilibrium_get_n_grad
+   PROCEDURE, PASS :: get_t => slab_equilibrium_get_t
+   PROCEDURE, PASS :: get_t_grad => slab_equilibrium_get_t_grad
+   FINAL           :: slab_equilibrium_destruct
 END TYPE
 
+!*******************************************************************************
+!  INTERFACE BLOCKS
+!*******************************************************************************
+!-------------------------------------------------------------------------------
+!>  Interface for the slab_equilibrium constructor.
+!-------------------------------------------------------------------------------
+INTERFACE slab_equilibrium_class
+   MODULE PROCEDURE slab_equilibrium_construct
+END INTERFACE
+
 CONTAINS
+!*******************************************************************************
+!  CONSTRUCTION SUBROUTINES
+!*******************************************************************************
+!-------------------------------------------------------------------------------
+!>  @brief Construct a default @ref custom_equilibrium_class object.
+!>
+!>  This is a hello world example of constructing a ways class.
+!>
+!>  @param[in] ne_scale Scale factor for density.
+!>  @param[in] te_scale Scale factor for temperature.
+!>  @returns A pointer to a constructed @ref custom_equilibrium_class object.
+!-------------------------------------------------------------------------------
+FUNCTION slab_equilibrium_construct()
+
+IMPLICIT NONE
+
+!  Declare Arguments
+CLASS (slab_equilibrium_class), POINTER :: slab_equilibrium_construct
+
+!  Start of executable code
+ALLOCATE (slab_equilibrium_construct)
+
+END FUNCTION
 
 !*******************************************************************************
 !  DESTRUCTION SUBROUTINES
@@ -42,12 +74,12 @@ CONTAINS
 !>
 !>  @param[inout] this A @ref equilibrium_interface_class instance.
 !-------------------------------------------------------------------------------
-SUBROUTINE equilibrium_interface_destruct(this)
+SUBROUTINE slab_equilibrium_destruct(this)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-TYPE (equilibrium_interface_class), INTENT(inout) :: this
+TYPE (slab_equilibrium_class), INTENT(inout) :: this
 
 !  Start of executable code
 
@@ -67,20 +99,20 @@ END SUBROUTINE
 !>  @param[in]    z    Z position.
 !>  @returns The magnetic field vector.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_b_vec(this, x, y, z)
+FUNCTION slab_equilibrium_get_b_vec(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE), DIMENSION(3) :: equilibrium_interface_get_b_vec
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
-REAL(C_DOUBLE), INTENT(in)                         :: x
-REAL(C_DOUBLE), INTENT(in)                         :: y
-REAL(C_DOUBLE), INTENT(in)                         :: z
+REAL(C_DOUBLE), DIMENSION(3) :: slab_equilibrium_get_b_vec
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
+REAL(C_DOUBLE), INTENT(in)                    :: x
+REAL(C_DOUBLE), INTENT(in)                    :: y
+REAL(C_DOUBLE), INTENT(in)                    :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_b_vec method not implimented.')
+slab_equilibrium_get_b_vec = (/ 1.0, 0.0, 0.0 /)
 
 END FUNCTION
 
@@ -96,20 +128,20 @@ END FUNCTION
 !>  @param[in]    z    Z position.
 !>  @returns The magnetic field jacobian.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_b_mag_jac(this, x, y, z)
+FUNCTION slab_equilibrium_get_b_mag_jac(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE), DIMENSION(3,3) :: equilibrium_interface_get_b_mag_jac
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
-REAL(C_DOUBLE), INTENT(in)                         :: x
-REAL(C_DOUBLE), INTENT(in)                         :: y
-REAL(C_DOUBLE), INTENT(in)                         :: z
+REAL(C_DOUBLE), DIMENSION(3,3) :: slab_equilibrium_get_b_mag_jac
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
+REAL(C_DOUBLE), INTENT(in)                    :: x
+REAL(C_DOUBLE), INTENT(in)                    :: y
+REAL(C_DOUBLE), INTENT(in)                    :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_b_mag_jac method not implimented.')
+slab_equilibrium_get_b_mag_jac = 0.0
 
 END FUNCTION
 
@@ -124,20 +156,20 @@ END FUNCTION
 !>  @param[in]    z    Z position.
 !>  @returns The density.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_n(this, x, y, z)
+FUNCTION slab_equilibrium_get_n(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE) :: equilibrium_interface_get_n
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
+REAL(C_DOUBLE) :: slab_equilibrium_get_n
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
 REAL(C_DOUBLE), INTENT(in)                         :: x
 REAL(C_DOUBLE), INTENT(in)                         :: y
 REAL(C_DOUBLE), INTENT(in)                         :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_n method not implimented.')
+slab_equilibrium_get_n = 1.0
 
 END FUNCTION
 
@@ -152,20 +184,20 @@ END FUNCTION
 !>  @param[in]    z    Z position.
 !>  @returns The density gradient.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_n_grad(this, x, y, z)
+FUNCTION slab_equilibrium_get_n_grad(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE), DIMENSION(3) :: equilibrium_interface_get_n_grad
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
-REAL(C_DOUBLE), INTENT(in)                         :: x
-REAL(C_DOUBLE), INTENT(in)                         :: y
-REAL(C_DOUBLE), INTENT(in)                         :: z
+REAL(C_DOUBLE), DIMENSION(3) :: slab_equilibrium_get_n_grad
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
+REAL(C_DOUBLE), INTENT(in)                    :: x
+REAL(C_DOUBLE), INTENT(in)                    :: y
+REAL(C_DOUBLE), INTENT(in)                    :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_n_grad method not implimented.')
+slab_equilibrium_get_n_grad = 0.0
 
 END FUNCTION
 
@@ -180,20 +212,20 @@ END FUNCTION
 !>  @param[in]    z    Z position.
 !>  @returns The temperature.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_t(this, x, y, z)
+FUNCTION slab_equilibrium_get_t(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE) :: equilibrium_interface_get_t
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
-REAL(C_DOUBLE), INTENT(in)                         :: x
-REAL(C_DOUBLE), INTENT(in)                         :: y
-REAL(C_DOUBLE), INTENT(in)                         :: z
+REAL(C_DOUBLE) :: slab_equilibrium_get_t
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
+REAL(C_DOUBLE), INTENT(in)                    :: x
+REAL(C_DOUBLE), INTENT(in)                    :: y
+REAL(C_DOUBLE), INTENT(in)                    :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_t method not implimented.')
+slab_equilibrium_get_t = 1.0
 
 END FUNCTION
 
@@ -208,20 +240,20 @@ END FUNCTION
 !>  @param[in]    z    Z position.
 !>  @returns The temperature gradient.
 !-------------------------------------------------------------------------------
-FUNCTION equilibrium_interface_get_t_grad(this, x, y, z)
+FUNCTION slab_equilibrium_get_t_grad(this, x, y, z)
 
 IMPLICIT NONE
 
 !  Declare Arguments
-REAL(C_DOUBLE), DIMENSION(3) :: equilibrium_interface_get_t_grad
-CLASS (equilibrium_interface_class), INTENT(inout) :: this
-REAL(C_DOUBLE), INTENT(in)                         :: x
-REAL(C_DOUBLE), INTENT(in)                         :: y
-REAL(C_DOUBLE), INTENT(in)                         :: z
+REAL(C_DOUBLE), DIMENSION(3) :: slab_equilibrium_get_t_grad
+CLASS (slab_equilibrium_class), INTENT(inout) :: this
+REAL(C_DOUBLE), INTENT(in)                    :: x
+REAL(C_DOUBLE), INTENT(in)                    :: y
+REAL(C_DOUBLE), INTENT(in)                    :: z
 
 !  Start of executable code
 
-CALL assert(.FALSE., 'get_t_grad method not implimented.')
+slab_equilibrium_get_t_grad = 0.0
 
 END FUNCTION
 
